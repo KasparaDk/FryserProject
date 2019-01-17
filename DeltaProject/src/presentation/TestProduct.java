c2ff67ddd32c70833a3292d3bbbf3571ed5fa7de
Kasper <Kasper@10.153.238.35>
2019-01-17 09:20:08 +0100

    Dummy data

--- DeltaProject/src/presentation/TestProduct.java
+++ DeltaProject/src/presentation/TestProduct.java
@@ -10,17 +10,29 @@ import logic.ProductType;
 
 public class TestProduct {
 	
-//	Product test = new Product(0, "lort", "på", "09/03/2034", ProductType.BEEF, "godt");
-	Product test = new Product(0, "Pølse", "15/09/2018", "500g",ProductType.SAUSAGE, "Nederste hylde");
-	Product test2 = new Product(0, "Kylling", "02/04/2018", "2000g", ProductType.CHICKEN, "Midterste hylde");
-	Product testupdate = new Product(2, "Update", "01/01/3019", "Update", ProductType.FRUIT, "En Update");
+	Product test = new Product(0, "sovs", "på", "09/03/2034", ProductType.BEEF, "godt");
+	Product test1 = new Product(0, "Pølse", "15/09/2018", "500g",ProductType.SAUSAGE, "Nederste hylde");
+	Product test2 = new Product(0, "Kylling", "15/02/2016", "2000g", ProductType.CHICKEN, "Midterste hylde");
+	Product test3 = new Product(0, "Bøff", "12/12/2053", "2344g", ProductType.FATFISH, "Sove hylde");
+	Product test4 = new Product(0, "Is", "31/07/2013", "100g", ProductType.SMOKEDMEAT, "Midterste hul");
+	Product test5 = new Product(0, "Suppe", "01/02/1995", "1234g", ProductType.MINCEDMEAT, "Et Hemmeligt sted");
+	Product test6 = new Product(0, "Jeg ved ikke hvad", "15/10/1980", "5000g", ProductType.LAMB, "Sjove ting");
+	Product test7 = new Product(0, "ØL", "06/02/2020", "2000g", ProductType.MINCEDFISH, "Jeg vil sove");
+	Product testupdate = new Product(2, "opdater", "01/01/2020", "opdater", ProductType.BEEF, "opdater");
+
 	
 	ProductController productController = new ProductController(DatabaseConnection.newConnection("JanProjectDB"));
 	CheckDate checkDate = new CheckDate();
 	
 	public void add() {
+		productController.addProduct(test);
+		productController.addProduct(test1);
 		productController.addProduct(test2);
-
+		productController.addProduct(test3);
+		productController.addProduct(test4);
+		productController.addProduct(test5);
+		productController.addProduct(test6);
+		productController.addProduct(test7);
 	}
 	public void getAll() {
 		List<Product> product2 = productController.getAllProducts();
