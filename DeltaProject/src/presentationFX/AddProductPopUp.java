a73d12c51c9271adfe74ea89d6c3f7e626d1a750
Bruger <Bruger@10.153.238.52>
2019-01-17 10:51:29 +0100

    Ændring i layout og Comboboxen

--- DeltaProject/src/presentationFX/AddProductPopUp.java
+++ DeltaProject/src/presentationFX/AddProductPopUp.java
@@ -6,25 +6,24 @@ import java.time.format.DateTimeFormatter;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Tooltip;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import logic.DatabaseConnection;
 import logic.Product;
 import logic.ProductController;
 import logic.ProductType;
 
 public class AddProductPopUp {
 
-	private ProductController productController;
+	private ProductController productController = new ProductController(DatabaseConnection.newConnection("JanProjectDB"));
 	private TextField varenavntxt;
 	private TextField indkøbsdatotxt;
 	private TextField mængdetxt;
@@ -66,63 +65,74 @@ public class AddProductPopUp {
 //		buttonVBox.getChildren().addAll(tilføj, annuller);
 
 		// Teksfelterne
+		//Vbokse til at indsætte tingene
+		VBox column1 = new VBox();
+		VBox column2 = new VBox();
+		
 		// Varenavn
 		Label varenavnlbl = new Label("Varenavn:");
 		varenavntxt = new TextField();
-		varenavntxt.setMaxWidth(100);
-		HBox varenavnbox = new HBox();
-		varenavnbox.getChildren().addAll(varenavnlbl, varenavntxt);
-//		varenavnbox.setSpacing(10);
+		varenavntxt.setMaxWidth(150);
+//		HBox varenavnbox = new HBox();
+//		varenavnbox.getChildren().addAll(varenavnlbl, varenavntxt);
 
 		// Varetype
 		Label varetypelbl = new Label("Varetype:");
 		// Dropdown menu med varetyper
-		ComboBox<ProductType> cmb = new ComboBox<>();
-		cmb.setTooltip(new Tooltip());
+//		ComboBox<ProductType> 
+		cmb = new ComboBox<>();
+//		cmb.setTooltip(new Tooltip());
 		cmb.getItems().addAll(ProductType.values());
-		HBox varetypebox = new HBox();
-		varetypebox.getChildren().addAll(varetypelbl, cmb);
+		cmb.setMinWidth(150);
+//		HBox varetypebox = new HBox();
+//		varetypebox.getChildren().addAll(varetypelbl, cmb);
 
 		// Indkøbsdato
 		Label indkøbsdatolbl = new Label("Indkøbsdato:");
 		indkøbsdatotxt = new TextField();
-		indkøbsdatotxt.setMaxWidth(100);
+		indkøbsdatotxt.setMaxWidth(150);
 		// Gør tekstfeltet uneditable
 		indkøbsdatotxt.setEditable(false);
 		indkøbsdatotxt.setMouseTransparent(true);
 		indkøbsdatotxt.setFocusTraversable(false);
-		String date = LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+		String date = LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MMMM - yyyy"));
 		indkøbsdatotxt.setText(date);
 		indkøbsdatotxt.setStyle("-fx-text-inner-color: grey;");
-		HBox indkøbsdatobox = new HBox();
-		indkøbsdatobox.getChildren().addAll(indkøbsdatolbl, indkøbsdatotxt);
+		indkøbsdatotxt.setAlignment(Pos.CENTER);
+//		HBox indkøbsdatobox = new HBox();
+//		indkøbsdatobox.getChildren().addAll(indkøbsdatolbl, indkøbsdatotxt);
 
 		// Mængde
 		Label mængdelbl = new Label("Mændge:");
 		mængdetxt = new TextField();
-		HBox mængdebox = new HBox();
-		mængdebox.getChildren().addAll(mængdelbl, mængdetxt);
+		mængdetxt.setMaxWidth(150);
+//		HBox mængdebox = new HBox();
+//		mængdebox.getChildren().addAll(mængdelbl, mængdetxt);
 
 		// Note
 		Label notelbl = new Label("Note:");
 		notetxt = new TextField();
-		notetxt.setMaxWidth(200);
-
-		HBox notebox = new HBox();
-		notebox.getChildren().addAll(notelbl, notetxt);
-		notebox.setSpacing(10);
-
+		notetxt.setMaxWidth(150);
+//		HBox notebox = new HBox();
+//		notebox.getChildren().addAll(notelbl, notetxt);
+//		notebox.setSpacing(10);
+		
+		//Indsætter tingene i Vboksene
+		column1.getChildren().addAll(varenavnlbl, varetypelbl, indkøbsdatolbl, mængdelbl, notelbl);
+		column1.setSpacing(20);
+		column1.setAlignment(Pos.CENTER_RIGHT);
+		
+		column2.getChildren().addAll(varenavntxt, cmb, indkøbsdatotxt, mængdetxt, notetxt);
+		column2.setSpacing(12);
+		column2.setPadding(new Insets(0, 0, 0, 0));
+		column2.setAlignment(Pos.CENTER_RIGHT);
+		
 		// GridPane med textfelterne
-		VBox textFields = new VBox();
-		textFields.setAlignment(Pos.CENTER_RIGHT);
-		textFields.getChildren().addAll(varenavnbox, varetypebox, indkøbsdatobox, mængdebox, notebox);
-
-//		textFields.add(varenavnbox, 0, 0);
-//		textFields.add(varetypebox, 0, 1);
-//		textFields.add(indkøbsdatobox, 0, 2);
-//		textFields.add(notebox, 0, 3);
-//		textFields.setHgap(20);
-//		textFields.setVgap(20);
+		GridPane textFields = new GridPane();
+		textFields.getColumnConstraints().add(new ColumnConstraints(110));
+		textFields.getColumnConstraints().add(new ColumnConstraints(182));
+		textFields.add(column1, 0, 0);
+		textFields.add(column2, 1, 0);
 
 		BorderPane layout = new BorderPane();
 		layout.setPadding(new Insets(20, 20, 10, 20));
@@ -135,14 +145,14 @@ public class AddProductPopUp {
 		popUp.setScene(scene);
 		popUp.show();
 
-		new Dropdown<ProductType>(cmb);
+//		new Dropdown<ProductType>(cmb);
 	}
 
 	private void addProcuct() {
 		Product product = new Product(0, varenavntxt.getText(), indkøbsdatotxt.getText(), mængdetxt.getText(), cmb.getValue(), notetxt.getText());
 		productController.addProduct(product);
-		Alert alert = new Alert(AlertType.INFORMATION, "Product" + product + "oprettet", ButtonType.OK);
-		alert.showAndWait();
+//		Alert alert = new Alert(AlertType.INFORMATION, "Product" + product + "oprettet", ButtonType.OK);
+//		alert.showAndWait();
 		varenavntxt.clear();
 		mængdetxt.clear();
 		notetxt.clear();
