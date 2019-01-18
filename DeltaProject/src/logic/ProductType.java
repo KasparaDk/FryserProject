7d0dbffe7919058a275b04581ff4c1894b0daf66
Rasmus <Rasmus@10.153.170.17>
2019-01-18 13:54:30 +0100

    Færdigretter tilføjet

--- DeltaProject/src/logic/ProductType.java
+++ DeltaProject/src/logic/ProductType.java
@@ -2,7 +2,7 @@ package logic;
 
 public enum ProductType {
 	MINCEDMEAT(4, "Fars"), FATFISH(3, "Fisk, Fed"), MINCEDFISH(2, "Fisk, Hakket"), LEANFISH(6, "Fisk, Magert"), 
-	FRUIT(12, "Frugt"), VEGETABLES(10, "Grøntsager"), CHICKEN(10, "Kylling"), LAMB(10, "Lammekød"), BEEF(10, "Oksekød"), 
+	FRUIT(12, "Frugt"), READYMEAL(3, "Færdig Ret"), VEGETABLES(10, "Grøntsager"), CHICKEN(10, "Kylling"), LAMB(10, "Lammekød"), BEEF(10, "Oksekød"), 
 	SAUSAGE(3, "Pølse"), SMOKEDMEAT(6, "Røget kød"), FATPORK(3, "Svinekød, Fed"), LEANPORK(6, "Svinekød, Magert");
 
 	private int months;
