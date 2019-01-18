09ae47851384f5d7356f0f43291acb35414963e1
Rasmus <Rasmus@10.153.170.17>
2019-01-18 13:56:52 +0100

    hej

--- DeltaProject/src/logic/ProductType.java
+++ DeltaProject/src/logic/ProductType.java
@@ -1,29 +1,29 @@
 package logic;
 
 public enum ProductType {
-	MINCEDMEAT(4, "Fars"), FATFISH(3, "Fisk, Fed"), MINCEDFISH(2, "Fisk, Hakket"), LEANFISH(6, "Fisk, Magert"), 
-	FRUIT(12, "Frugt"), READYMEAL(3, "Færdig Ret"), VEGETABLES(10, "Grøntsager"), CHICKEN(10, "Kylling"), LAMB(10, "Lammekød"), BEEF(10, "Oksekød"), 
-	SAUSAGE(3, "Pølse"), SMOKEDMEAT(6, "Røget kød"), FATPORK(3, "Svinekød, Fed"), LEANPORK(6, "Svinekød, Magert");
+	MINCEDMEAT(4, "Fars"), FATFISH(3, "Fisk, Fed"), MINCEDFISH(2, "Fisk, Hakket"), LEANFISH(6, "Fisk, Magert"),
+	FRUIT(12, "Frugt"), READYMEAL(3, "Færdig Ret"), VEGETABLES(10, "Grøntsager"), CHICKEN(10, "Kylling"),
+	LAMB(10, "Lammekød"), BEEF(10, "Oksekød"), SAUSAGE(3, "Pølse"), SMOKEDMEAT(6, "Røget kød"),
+	FATPORK(3, "Svinekød, Fed"), LEANPORK(6, "Svinekød, Magert");
 
 	private int months;
 	private String danishType;
 
-ProductType(int months, String danishType) {
-	this.months = months;
-	this.danishType = danishType;
-}
+	ProductType(int months, String danishType) {
+		this.months = months;
+		this.danishType = danishType;
+	}
 
-public int getMonths() {
-	return months;
-}
+	public int getMonths() {
+		return months;
+	}
 
+	public String getDanishType() {
+		return danishType;
+	}
 
-public String getDanishType() {
-	return danishType;
-}
-
-@Override
-public String toString() {
-	return danishType;
-}
+	@Override
+	public String toString() {
+		return danishType;
+	}
 }
