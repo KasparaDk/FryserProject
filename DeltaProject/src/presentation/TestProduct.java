29353d81b3cabd31dbb6ca0c91dace68fcaf36c0
Kasper <Kasper@10.153.238.12>
2019-01-21 12:39:28 +0100

    Dummydata

--- DeltaProject/src/presentation/TestProduct.java
+++ DeltaProject/src/presentation/TestProduct.java
@@ -13,12 +13,12 @@ public class TestProduct {
 	
 	Product test = new Product(0, "sovs", LocalDate.of(2018, 3, 21), "500g", ProductType.BEEF, "godt");
 	Product test1 = new Product(0, "Pølse", LocalDate.of(2018, 11, 1), "500g",ProductType.SAUSAGE, "Nederste hylde");
-//	Product test2 = new Product(0, "Kylling", "25/03/2018", "2000g", ProductType.CHICKEN, "Midterste hylde");
-//	Product test3 = new Product(0, "Bøff", "12/12/2053", "2344g", ProductType.FATFISH, "Sove hylde");
-//	Product test4 = new Product(0, "Is", "31/07/2013", "100g", ProductType.SMOKEDMEAT, "Midterste hul");
-//	Product test5 = new Product(0, "Suppe", "01/02/1995", "1234g", ProductType.MINCEDMEAT, "Et Hemmeligt sted");
-//	Product test6 = new Product(0, "Jeg ved ikke hvad", "15/10/1980", "5000g", ProductType.LAMB, "Sjove ting");
-//	Product test7 = new Product(0, "ØL", "06/02/2020", "2000g", ProductType.MINCEDFISH, "Jeg vil sove");
+	Product test2 = new Product(0, "Kylling", LocalDate.of(2018, 12, 18), "2000g", ProductType.CHICKEN, "Midterste hylde");
+	Product test3 = new Product(0, "Bøff", LocalDate.of(2018, 10, 21), "2344g", ProductType.FATFISH, "Sove hylde");
+	Product test4 = new Product(0, "Is", LocalDate.of(2018, 9, 17), "100g", ProductType.SMOKEDMEAT, "Midterste hul");
+	Product test5 = new Product(0, "Suppe", LocalDate.of(2019, 1, 13), "1234g", ProductType.MINCEDMEAT, "Et Hemmeligt sted");
+	Product test6 = new Product(0, "Jeg ved ikke hvad", LocalDate.of(2018, 8, 21), "5000g", ProductType.LAMB, "Sjove ting");
+	Product test7 = new Product(0, "ØL", LocalDate.of(2018, 12, 13), "2000g", ProductType.MINCEDFISH, "Jeg vil sove");
 //	Product testupdate = new Product(2, "opdater", "01/01/2020", "opdater", ProductType.BEEF, "opdater");
 
 	
@@ -28,12 +28,12 @@ public class TestProduct {
 	public void add() {
 		productController.addProduct(test);
 		productController.addProduct(test1);
-//		productController.addProduct(test2);
-//		productController.addProduct(test3);
-//		productController.addProduct(test4);
-//		productController.addProduct(test5);
-//		productController.addProduct(test6);
-//		productController.addProduct(test7);
+		productController.addProduct(test2);
+		productController.addProduct(test3);
+		productController.addProduct(test4);
+		productController.addProduct(test5);
+		productController.addProduct(test6);
+		productController.addProduct(test7);
 	}
 	public void getAll() {
 		List<Product> product2 = productController.getAllProducts();
