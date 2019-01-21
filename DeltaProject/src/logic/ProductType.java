65f60af3a15f8a1adf18b41325cd383e9e990aa9
Rasmus <Rasmus@10.153.170.17>
2019-01-21 14:20:06 +0100

    tilføjet maaaaaaange flere typer

--- DeltaProject/src/logic/ProductType.java
+++ DeltaProject/src/logic/ProductType.java
@@ -1,10 +1,13 @@
 package logic;
 
 public enum ProductType {
-	MINCEDMEAT(4, "Fars"), FATFISH(3, "Fisk, Fed"), MINCEDFISH(2, "Fisk, Hakket"), LEANFISH(6, "Fisk, Magert"),
-	FRUIT(12, "Frugt"), READYMEAL(3, "Færdig Ret"), VEGETABLES(10, "Grøntsager"), CHICKEN(10, "Kylling"),
-	LAMB(10, "Lammekød"), BEEF(10, "Oksekød"), SAUSAGE(3, "Pølse"), SMOKEDMEAT(6, "Røget kød"),
-	FATPORK(3, "Svinekød, Fed"), LEANPORK(6, "Svinekød, Magert");
+	BABYFOOD(2, "Babymos"), BREAD(6, "Brød"), MINCEDMEAT(4, "Fars"), FATFISH(3, "Fisk, Fed"),
+	MINCEDFISH(2, "Fisk, Hakket"), LEANFISH(6, "Fisk, Magert"), CREAM(4, "Fløde"), ICECREAM(2, "Flødeis"),
+	FROMAGE(3, "Fromage/Mousse"), FRUIT(12, "Frugt"), READYMEAL(3, "Hjemmelavet ret"), VEGETABLES(10, "Grøntsager"),
+	BOILEDANDFRIEDFOWL(2, "Kogt og stegt fjerkræ"), CHICKEN(10, "Kylling"), LAMB(10, "Lammekød"), MILK(4, "Mælk"),
+	BEEF(10, "Oksekød"), CHEESE45(4, "Ost, Fed(45+)"), GRATEDCHEESE(4, "Revet ost"), SAUSAGE(3, "Pølse"),
+	SMOKEDMEAT(6, "Røget kød"), SEAFOOD(3, "Skaldyr"), BUTTER(3, "Smør"), MUSHROOM(5, "Svampe(Ristet i fedtstof)"),
+	FATPORK(3, "Svinekød, Fed"), LEANPORK(6, "Svinekød, Magert"), EGGYOLK(6, "Æggeblommer"), EGGWHITES(8, "Æggehvider");
 
 	private int months;
 	private String danishType;
