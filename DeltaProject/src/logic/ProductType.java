b1e3bed275d278901d888532acc07d0b6768f2cf
Rasmus <Rasmus@10.153.170.53>
2019-01-22 10:15:06 +0100

    alfabetisk

--- DeltaProject/src/logic/ProductType.java
+++ DeltaProject/src/logic/ProductType.java
@@ -6,12 +6,12 @@ public enum ProductType {
 	LEANFISH(6, "Fisk, Magert"), CREAM(4, "Fløde"), ICECREAM(2, "Flødeis"), FROMAGE(3, "Fromage/Mousse"),
 	FRUIT(12, "Frugt"), READYMEAL(3, "Hjemmelavet ret"), CAKE(3, "Kage"), VEGETABLES(10, "Grøntsager"),
 	CARROT(12, "Gulerod"), BOILEDANDFRIEDFOWL(2, "Kogt og stegt fjerkræ"), CHICKEN(10, "Kylling"), LAMB(10, "Lammekød"),
-	MILK(4, "Mælk"), BEEF(10, "Oksekød"), CHEESE45(4, "Ost, Fed(45+)"), GRATEDCHEESE(4, "Revet ost"),
-	SAUSAGE(3, "Pølse"), SMOKEDMEAT(6, "Røget kød"), SEAFOOD(3, "Skaldyr"), BUTTER(3, "Smør"),
+	MILK(4, "Mælk"), BEEF(10, "Oksekød"), CHEESE45(4, "Ost, Fed(45+)"), SAUSAGE(3, "Pølse"),
+	GRATEDCHEESE(4, "Revet ost"), SMOKEDMEAT(6, "Røget kød"), SEAFOOD(3, "Skaldyr"), BUTTER(3, "Smør"),
 	MUSHROOM(5, "Svampe(Ristet i fedtstof)"), FATPORK(3, "Svinekød, Fed"), LEANPORK(6, "Svinekød, Magert"),
 	TOMATO(12, "Tomat"), YOGHURT(2, "Yoghurt"), APPLE(8, "Æbler"), EGGYOLK(6, "Æggeblommer"),
 	EGGWHITES(8, "Æggehvider");
-
+	
 	private int months;
 	private String danishType;
 
